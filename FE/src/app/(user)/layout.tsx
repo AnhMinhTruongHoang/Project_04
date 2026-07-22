7ac20b07147cff59bb4091b85ea3a5268eed9429
AnhMinh <truonghoanganhminh2000@gmail.com>
2026-07-23 01:49:27 +0700

    redeploy with nextjs

--- FE/src/app/(user)/layout.tsx
+++ FE/src/app/(user)/layout.tsx
@@ -1,54 +1,19 @@
 import AppHeader from "@/components/header/app.header";
 import AppOpenAds from "@/components/common/app.open.ads";
-import type { Metadata } from "next";
-import Script from "next/script";
+import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Tiêu đề from layout",
-  description: "miêu tả layout thôi mà",
-};
+  title: "SoundClone",
+  description: "Discover, stream, and share music on SoundClone.",
 
-const test = {
-  "@context": "https://schema.org",
-  "@type": "Store",
-  name: "Tiki",
-  image: {
-    "@type": "ImageObject",
-    url: "https://salt.tikicdn.com/cache/w500/ts/upload/c0/8b/46/c3f0dc850dd93bfa7af7ada0cbd75dc0.png",
-    width: 1080,
-    height: 1080,
-  },
-  telephone: "19006035",
-  url: "https://tiki.vn/",
-  address: {
-    "@type": "PostalAddress",
-    streetAddress: "52 Ut Tich, Ward 4, Tan Binh District, Ho Chi Minh City",
-    addressLocality: "Ho Chi Minh",
-    postalCode: "700000",
-    addressRegion: "Ho Chi Minh",
-    addressCountry: "VN",
-  },
-  priceRange: "1000 - 1000000000",
-  openingHoursSpecification: [
-    {
-      "@type": "OpeningHoursSpecification",
-      dayOfWeek: [
-        "Monday",
-        "Tuesday",
-        "Wednesday",
-        "Thursday",
-        "Friday",
-        "Saturday",
-        "Sunday",
-      ],
-      opens: "08:00",
-      closes: "21:00",
-    },
-  ],
-  geo: {
-    "@type": "GeoCoordinates",
-    latitude: "10.79664498748942",
-    longitude: "106.65856519879867",
+  openGraph: {
+    title: "SoundClone",
+    description: "Discover, stream, and share music on SoundClone.",
+    images: [
+      {
+        url: "https://res.cloudinary.com/eybmkz9z/image/upload/v1784726300/default_djtlyj.png",
+      },
+    ],
   },
 };
 
@@ -64,11 +29,6 @@ export default function RootLayout({
       {children}
 
       <AppOpenAds />
-
-      <Script
-        type="application/ld+json"
-        dangerouslySetInnerHTML={{ __html: JSON.stringify(test) }}
-      />
     </>
   );
 }

f1775a0c6112fd339e9dc28dd2e53cab811f8a69
HungLam2801 <lamxuanhung01282004@gmail.com>
2026-08-26 09:57:47 +0700

    Done Merge

--- Mobile/macos/Flutter/GeneratedPluginRegistrant.swift
+++ Mobile/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,18 +5,18 @@
 import FlutterMacOS
 import Foundation
 
-import file_selector_macos
 import audio_session
+import file_selector_macos
 import flutter_secure_storage_darwin
+import just_audio
 import mobile_scanner
 import url_launcher_macos
-import just_audio
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   AudioSessionPlugin.register(with: registry.registrar(forPlugin: "AudioSessionPlugin"))
+  FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   FlutterSecureStorageDarwinPlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStorageDarwinPlugin"))
+  JustAudioPlugin.register(with: registry.registrar(forPlugin: "JustAudioPlugin"))
   MobileScannerPlugin.register(with: registry.registrar(forPlugin: "MobileScannerPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
-  JustAudioPlugin.register(with: registry.registrar(forPlugin: "JustAudioPlugin"))
 }
